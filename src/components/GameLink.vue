125cc76e043b6e63a7b61c4e89190d5bd931a2c2
Timeuh <timeuuh@gmail.com>
2025-12-16 16:10:42 +0100

    ✨ create component for a link to a game page

---
+++ src/components/GameLink.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+
+const props = defineProps({
+  text: String,
+  link: String,
+})
+</script>
+
+<template>
+  <RouterLink :to="link">
+    <slot />
+    <h2>{{ text }}</h2>
+  </RouterLink>
+</template>
