e023bc0225e9d839b281f036c3b51c0d3fd3d1ea
Timeuh <timeuuh@gmail.com>
2025-12-16 15:41:32 +0100

    🔧 add eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import { defineConfig, globalIgnores } from 'eslint/config'
 import globals from 'globals'
 import js from '@eslint/js'
 import pluginVue from 'eslint-plugin-vue'
+import perfectionist from 'eslint-plugin-perfectionist'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 
 export default defineConfig([
@@ -14,13 +15,56 @@ export default defineConfig([
 
   {
     languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
       globals: {
         ...globals.browser,
+        ...globals.node,
       },
     },
   },
 
   js.configs.recommended,
-  ...pluginVue.configs['flat/essential'],
+
+  ...pluginVue.configs['flat/vue3-recommended'],
+
+  {
+    plugins: {
+      vue: pluginVue,
+      perfectionist,
+    },
+
+    rules: {
+      'vue/order-in-components': 'error',
+      'vue/component-tags-order': [
+        'error',
+        {
+          order: ['script', 'template', 'style'],
+        },
+      ],
+      'vue/attributes-order': 'error',
+      'vue/no-ref-as-operand': 'error',
+      'vue/no-setup-props-destructure': 'error',
+      'vue/prefer-import-from-vue': 'error',
+      'perfectionist/sort-imports': [
+        'error',
+        {
+          type: 'natural',
+          order: 'asc',
+          newlinesBetween: 'always',
+        },
+      ],
+      'perfectionist/sort-objects': [
+        'error',
+        {
+          type: 'natural',
+          order: 'asc',
+        },
+      ],
+      'perfectionist/sort-exports': 'error',
+
+      'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    },
+  },
   skipFormatting,
 ])
