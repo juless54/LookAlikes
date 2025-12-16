3ba570332a0a549005a8142ae087c7fdaf13f3c7
Timeuh <timeuuh@gmail.com>
2025-12-16 16:10:54 +0100

    ✨ display links for different games

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,15 @@
-<script setup></script>
+<script setup>
+import GameLink from '@/components/GameLink.vue'
+</script>
 
 <template>
   <main>
     <h1 class="text-2xl text-red-500">LookAlikes</h1>
+    <GameLink link="/imagesGame" text="Jouer avec les images">
+      <h3>Image</h3>
+    </GameLink>
+    <GameLink link="/wordsGame" text="Jouer avec les mots">
+      <h3>Mots</h3>
+    </GameLink>
   </main>
 </template>
