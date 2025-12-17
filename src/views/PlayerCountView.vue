26a89454bfe82cf6bc612ec7eeebabc0ebc35d02
Timeuh <timeuuh@gmail.com>
2025-12-17 16:08:30 +0100

    🚧 create view to choose player count

---
+++ src/views/PlayerCountView.vue
@@ -0,0 +1,18 @@
+<script setup></script>
+
+<template>
+  <section>
+    <h1>Nombre de Joueurs</h1>
+    <div>
+      <div>
+        <h2>Innocents</h2>
+        <input type="number" />
+      </div>
+      <div>
+        <h2>Imposteurs</h2>
+        <input type="number" />
+      </div>
+    </div>
+    <button>Commencer</button>
+  </section>
+</template>
