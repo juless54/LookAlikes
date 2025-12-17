7408d98f49021f49b6f062ec5e0d9827e7dd73d0
Timeuh <timeuuh@gmail.com>
2025-12-17 16:47:19 +0100

    ✨ create app button component

---
+++ src/components/Button.vue
@@ -0,0 +1,16 @@
+<script setup>
+const props = defineProps({
+  text: String,
+})
+
+const emit = defineEmits(['click'])
+</script>
+
+<template>
+  <button
+    class="w-full bg-box shadow-gshadow/25 shadow-[0_0_2px_2px] rounded-xl p-2 font-kavoon text-xl"
+    @click="emit('click')"
+  >
+    {{ text }}
+  </button>
+</template>
