c890c87f3bc1c4109667abedc4d825c3c888dc8a
Timeuh <timeuuh@gmail.com>
2025-12-17 15:52:02 +0100

    ✨ create gamestate store

---
+++ src/stores/gamestate.js
@@ -0,0 +1,40 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+
+/**
+ * Store game state data
+ */
+export const useGameStateStore = defineStore('gamestate', () => {
+  // game state throughout the game
+  const gameState = ref('PlayerCount')
+
+  /**
+   * Swap to player names selection phase
+   */
+  function playerNamesPhase() {
+    gameState.value = 'PlayerNames'
+  }
+
+  /**
+   * Swap to game start phase
+   */
+  function gameStartPhase() {
+    gameState.value = 'GameStart'
+  }
+
+  /**
+   * Swap to game end phase
+   */
+  function gameEndPhase() {
+    gameState.value = 'GameEnd'
+  }
+
+  /**
+   * Swap back to player count phase
+   */
+  function resetGame() {
+    gameState.value = 'PlayerCount'
+  }
+
+  return { players, playerNamesPhase, gameStartPhase, gameEndPhase, resetGame }
+})
