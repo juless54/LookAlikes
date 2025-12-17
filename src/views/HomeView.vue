1b93e111610d68108e29a0bc6c08f860dcccafc0
Timeuh <timeuuh@gmail.com>
2025-12-17 14:46:29 +0100

    💄style home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,13 +3,17 @@ import GameLink from '@/components/GameLink.vue'
 </script>
 
 <template>
-  <main class="font-lao">
-    <h1 class="text-2xl text-red-500">LookAlikes</h1>
+  <main
+    class="font-lao bg-bg text-twhite flex flex-col items-center h-screen w-full justify-center space-y-10"
+  >
+    <h1 class="text-5xl font-kavoon">LookAlikes</h1>
     <GameLink link="/imagesGame" text="Jouer avec les images">
-      <h3>Image</h3>
+      <img src="/images/myosotis.png" alt="myosotis" class="h-[22vh] w-auto rounded-md" />
     </GameLink>
     <GameLink link="/wordsGame" text="Jouer avec les mots">
-      <h3>Mots</h3>
+      <div class="h-[20vh] bg-ubox rounded-md w-full flex flex-col items-center justify-center">
+        <h3 class="text-3xl font-kavoon">Banane</h3>
+      </div>
     </GameLink>
   </main>
 </template>
