3e476c32460bb70de8fbf4f631a4aa234fb49aab
Timeuh <timeuuh@gmail.com>
2025-12-17 14:46:17 +0100

    💄style game link

--- src/components/GameLink.vue
+++ src/components/GameLink.vue
@@ -8,8 +8,11 @@ const props = defineProps({
 </script>
 
 <template>
-  <RouterLink :to="link">
+  <RouterLink
+    :to="link"
+    class="rounded-xl bg-box shadow-gshadow/25 shadow-[0_0_2px_2px] p-6 flex flex-col space-y-6 items-center min-w-[60vw] min-h-[30vh]"
+  >
     <slot />
-    <h2>{{ text }}</h2>
+    <h2 class="text-xl">{{ text }}</h2>
   </RouterLink>
 </template>
