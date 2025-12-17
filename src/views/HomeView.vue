9722cf5fb26576ad739af05b42a1f42b20e87955
Timeuh <timeuuh@gmail.com>
2025-12-17 14:23:14 +0100

    💄use base font

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@ import GameLink from '@/components/GameLink.vue'
 </script>
 
 <template>
-  <main>
+  <main class="font-lao">
     <h1 class="text-2xl text-red-500">LookAlikes</h1>
     <GameLink link="/imagesGame" text="Jouer avec les images">
       <h3>Image</h3>
