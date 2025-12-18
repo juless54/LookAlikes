de713394dba07bc45624f72764d6e98924a1f94b
Timeuh <timeuuh@gmail.com>
2025-12-18 13:24:04 +0100

    ✨ display each player 1 by 1

--- src/views/GamePlayingView.vue
+++ src/views/GamePlayingView.vue
@@ -1,20 +1,31 @@
 <script setup>
 import { useGameStore } from '@/stores/game'
-import { useGameStateStore } from '@/stores/gamestate'
 import { storeToRefs } from 'pinia'
-import { onBeforeMount, onMounted, watch } from 'vue'
+import { computed, ref } from 'vue'
+import Button from '@/components/Button.vue'
 
 // load game data store
 const gameStore = useGameStore()
 const { playersWithRoles } = storeToRefs(gameStore)
+
+// current player index in roles array
+const currentPlayerIndex = ref(0)
+
+// current player
+const currentPlayer = computed(() => playersWithRoles.value[currentPlayerIndex.value])
+
+// swap to next player
+function nextPlayer() {
+  if (currentPlayerIndex.value === playersWithRoles.value.length - 1) return
+
+  currentPlayerIndex.value++
+}
 </script>
 
 <template>
   <section class="flex flex-col h-screen w-full bg-bg items-center text-twhite justify-center">
-    <h1 class="text-3xl font-kavoon">Jeu en cours</h1>
-    <div v-for="player in playersWithRoles">
-      <h2>{{ player.playerName }}</h2>
-      <h2>{{ player.playerRole }}</h2>
-    </div>
+    <h1 v-if="currentPlayer" class="text-3xl font-kavoon">Joueur {{ currentPlayer.playerName }}</h1>
+    <h2>Prenez le temps de bien regarder</h2>
+    <Button text="Joueur suivant" @click="nextPlayer()" />
   </section>
 </template>
