d47084e2608dfa4b402080e2d303fc7bf55522ea
Timeuh <timeuuh@gmail.com>
2025-12-18 14:46:24 +0100

    🚧 create view for player vote

--- src/views/ImagesGame.vue
+++ src/views/ImagesGame.vue
@@ -2,6 +2,7 @@
 import PlayerCountView from './PlayerCountView.vue'
 import PlayerNamesView from '@/views/PlayerNamesView.vue'
 import PlayerRoleRevealView from '@/views/PlayerRoleRevealView.vue'
+import PlayerVoteView from '@/views/PlayerVoteView.vue'
 import { useGameStateStore } from '@/stores/gamestate'
 import { storeToRefs } from 'pinia'
 
@@ -14,5 +15,6 @@ const { gameState } = storeToRefs(gameStateStore)
     <PlayerCountView v-show="gameState === 'PlayerCount'" />
     <PlayerNamesView v-show="gameState === 'PlayerNames'" />
     <PlayerRoleRevealView v-show="gameState === 'GameStart'" />
+    <PlayerVoteView v-show="gameState === 'GameVote'" />
   </main>
 </template>
---
+++ src/views/PlayerVoteView.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <section class="flex flex-col h-screen w-full bg-bg items-center text-twhite justify-center">
+    <div class="flex flex-col items-center justify-between h-[60vh] w-[80vw]">
+      <h1 class="text-3xl font-kavoon">Vote des joueurs</h1>
+    </div>
+  </section>
+</template>
