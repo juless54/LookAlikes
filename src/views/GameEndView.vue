71834a09b4d95eb0a381cba79ef94a4cf03d5000
Timeuh <timeuuh@gmail.com>
2025-12-19 15:15:31 +0100

    🚧 create game end view

---
+++ src/views/GameEndView.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <section
+    class="relative flex flex-col h-screen w-full bg-bg items-center text-twhite justify-center"
+  >
+    <div class="flex flex-col items-center justify-between h-[60vh] w-[80vw]">
+      <h1 class="text-3xl font-kavoon">Fin du jeu</h1>
+    </div>
+  </section>
+</template>
--- src/views/ImagesGame.vue
+++ src/views/ImagesGame.vue
@@ -3,6 +3,7 @@ import PlayerCountView from './PlayerCountView.vue'
 import PlayerNamesView from '@/views/PlayerNamesView.vue'
 import PlayerRoleRevealView from '@/views/PlayerRoleRevealView.vue'
 import PlayerVoteView from '@/views/PlayerVoteView.vue'
+import GameEndView from '@/views/GameEndView.vue'
 import { useGameStateStore } from '@/stores/gamestate'
 import { storeToRefs } from 'pinia'
 
@@ -16,5 +17,6 @@ const { gameState } = storeToRefs(gameStateStore)
     <PlayerNamesView v-show="gameState === 'PlayerNames'" />
     <PlayerRoleRevealView v-show="gameState === 'GameStart'" />
     <PlayerVoteView v-show="gameState === 'GameVote'" />
+    <GameEndView v-show="gameState === 'GameEnd'" />
   </main>
 </template>
